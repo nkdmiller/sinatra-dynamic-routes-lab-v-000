bb82380eb3f5338bba5c0ccecf7645f9bac56c96
Nate Miller <natekmiller96@gmail.com>
2018-08-12 18:30:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
   # This is a sample static route.
   get '/reversename/:name' do
     @user_name = params[:name].reverse
-    "@user_name"
+    "#{@user_name}"
   end
 
   # This is a sample dynamic route.
