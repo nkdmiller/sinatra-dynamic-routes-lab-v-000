f9e697114d649e61a0ab9e18dace67d09bd21aab
Nate Miller <natekmiller96@gmail.com>
2018-08-12 18:27:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
 
+  # This is a sample static route.
+  get '/reversename/:name' do
+    "Hello World!"
+  end
+
+  # This is a sample dynamic route.
+  get "/hello/:name" do
+    @user_name = params[:name]
+    "Hello #{@user_name}!"
+  end
+
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+  
+  get "/multiply/:num1/:num2" do
+    @number = params[:num1].to_i
+    @number2 = params[:num2].to_i
+    "#{@number * @number2}"
+  end
 end
