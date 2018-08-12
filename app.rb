61536e4fb14882f338dd3acefdaff0f79be36981
Nate Miller <natekmiller96@gmail.com>
2018-08-12 19:01:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -39,8 +39,14 @@ class App < Sinatra::Base
     if @operation = "add"
       "#{@number1 + @number2}"
     end
-    elsif @operation = "add"
-      "#{@number1 + @number2}"
+    elsif @operation = "subtract"
+      "#{@number1 - @number2}"
+    end
+    elsif @operation = "multiply"
+      "#{@number1 * @number2}"
+    end
+    elsif @operation = "divide"
+      "#{@number1 / @number2}"
     end
   end
 end
